732fc401e5d579e4b0e33f31a980b51493f96de9
jelany <j_robben92@hotmail.com>
2020-03-17 22:49:30 +0100

    start

---
+++ backend/messages/ar/app.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    //nav
+    'Categories'                           => 'فئة',
+    'Merchandise'                          => 'السلع',
+    'Article'                              => 'المنتج',
+    'Article Price'                        => 'سعر المنتج',
+    'Article Information'                  => 'معلومات المنتج',
+    'Market Information'                   => 'معلومات المتجر',
+    'Capital'                              => 'رأس مال',
+    'Purchase Invoices'                    => 'فواتير الشراء',
+    'Incoming Revenues'                    => 'الإيرادات الواردة',
+    'Purchases'                            => 'المشتريات',
+    'Market Expense'                       => 'نفقات المحل',
+    'Market Expenses'                      => 'نفقات المحل',
+    'Login'                                => 'تسجيل الدخول',
+    'Logout'                               => 'تسجيل الخروج',
+    'save'                                 => 'حفظ',
+    'Incoming Revenue'                     => 'الايراد اليومي',
+    'Create Incoming Revenue'              => 'انشاء الايراد اليومي',
+    'Create Market Expense'                => 'انشاء مصروفات ع المحل',
+    'Create Purchases'                     => 'انشاء مشتريات (بضائع)',
+    'Purchases export'                     => 'تصدير المشتريات',
+    'Daily Incoming Revenue'               => 'مبلغ الإيرادات الواردة',
+    'Selected Date'                        => 'التاريخ المحدد',
+    'Expense'                              => 'المبلغ',
+    'Reason'                               => 'السبب',
+    'Market expense was created for today' => 'تم انشاء مصروف للماركت لليوم',
+
+
+    //month
+    'March'                                => 'آذار',
+    'Total income for the month'           => 'إجمالي الدخل للشهر',
+];
