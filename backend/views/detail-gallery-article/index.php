843073cc419f91b39c3824998813bc8bb690ed34
jelany <j_robben92@hotmail.com>
2020-06-08 02:49:25 +0200

    test

--- backend/views/detail-gallery-article/index.php
+++ backend/views/detail-gallery-article/index.php
@@ -1,14 +1,31 @@
 <?php
 
-use yii\bootstrap4\Html;
 use common\components\GridView;
+use kartik\icons\Icon;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\Tabs;
 
 /* @var $this yii\web\View */
+/* @var $mainCategoryNames array */
 /* @var $searchModel common\models\searchModel\DetailGalleryArticlelSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title                   = Yii::t('app', 'Detail Gallery Articles');
 $this->params['breadcrumbs'][] = $this->title;
+$items                         = [];
+foreach ($mainCategoryNames as $mainCategoryName)
+{
+    $items[] = [
+        'label'       => Icon::show('list-alt') . ' ' . $mainCategoryName,
+        'linkOptions' => ['class' => 'tab-link'],
+        'active'      => Yii::$app->controller->id == 'detail-gallery-article' && Yii::$app->controller->action->id == 'index' && Yii::$app->controller->actionParams['mainCategoryName'] == $mainCategoryName,
+        'url'         => Yii::$app->urlManager->createUrl([
+                                                              'detail-gallery-article/index',
+                                                              'mainCategoryName' => $mainCategoryName,
+                                                          ]),
+        'encode'      => false,
+    ];
+}
 ?>
 <div class="detail-gallery-article-index">
 
@@ -17,6 +34,13 @@ $this->params['breadcrumbs'][] = $this->title;
         <p>
             <?= Html::a(Yii::t('app', 'Create Detail Gallery Article'), ['create'], ['class' => 'btn btn-success']) ?>
         </p>
+        <?php if (0 < count($items)) : ?>
+            <?= Tabs::widget([
+                                 'options' => ['id' => 'main_category_nav'],
+                                 'items'   => $items,
+                             ]); ?>
+        <br>
+        <?php endif; ?>
     <?php endif; ?>
     <?= GridView::widget([
                              'dataProvider' => $dataProvider,
@@ -27,11 +51,9 @@ $this->params['breadcrumbs'][] = $this->title;
                                  [
                                      'attribute' => 'author_name',
                                      'value'     => function ($model) {
-                                        return $model->bookGalleries->author_name;
+                                         return $model->bookGalleries->author_name;
                                      },
                                  ],
-                                 //'article_name_en',
-                                 //'link_to_preview:url',
                                  'selected_date',
                                  ['class' => 'yii\grid\ActionColumn'],
                              ],
