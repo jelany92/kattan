05dab7b0afdc6e2b1b92dce9ce3af6da5711f306
jelanyk <sosty.jojty0005@gmail.com>
2020-06-29 00:12:34 +0200

    test

--- backend/views/supermarket/tax-office/_form.php
+++ backend/views/supermarket/tax-office/_form.php
@@ -1,8 +1,9 @@
 <?php
 
-use yii\helpers\Html;
+use yii\bootstrap4\Html;
 use kartik\form\ActiveForm;
 use kartik\date\DatePicker;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\TaxOffice */
@@ -17,6 +18,20 @@ use kartik\date\DatePicker;
 
     <?= $form->field($model, 'income')->textInput() ?>
 
+    <?= $form->field($model, 'reason', [])->widget(Select2::class, [
+        'model'         => $model,
+        'attribute'     => 'reason',
+        'options'       => [
+            'placeholder' => 'Bitte auswählen ...',
+        ],
+        'pluginOptions' => [
+            'allowClear'         => false,
+            'tags'               => true,
+            'maximumInputLength' => false,
+        ],
+        'data'          => $reasonList,
+    ]) ?>
+    
     <?= $form->field($model, 'selected_date')->widget(DatePicker::class, [
         'options'       => ['placeholder' => 'Enter event time ...'],
         'pluginOptions' => [
--- backend/views/supermarket/tax-office/update.php
+++ backend/views/supermarket/tax-office/update.php
@@ -1,15 +1,25 @@
 <?php
 
-use yii\helpers\Html;
+use yii\bootstrap4\Html;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\TaxOffice */
+/* @var $reasonList array */
 
-$this->title = Yii::t('app', 'Update Tax Office: {name}', [
+$this->title                   = Yii::t('app', 'Update Tax Office: {name}', [
     'name' => $model->id,
 ]);
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Tax Office'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = [
+    'label' => Yii::t('app', 'Tax Office'),
+    'url'   => ['index'],
+];
+$this->params['breadcrumbs'][] = [
+    'label' => $model->id,
+    'url'   => [
+        'view',
+        'id' => $model->id,
+    ],
+];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="tax-office-update">
@@ -17,7 +27,8 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
-        'model' => $model,
+        'model'      => $model,
+        'reasonList' => $reasonList,
     ]) ?>
 
 </div>
